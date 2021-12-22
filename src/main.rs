1f3875117f735b0cd33cf234fb6fa3f7ce52bfd1
Karol Małecki <malecki.karol@outlook.com>
2021-12-22 18:03:42 +0100

    main.rs: created simple test for processor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::process::exit;
+
+use processor::{Processor, Instruction, Operand};
+
+mod memory;
+mod processor;
+
 fn main() {
-    println!("Hello, world!");
+    let mut processor = Processor::new(vec![
+        Instruction::Load(Operand::Immediate(3)),
+        Instruction::Store(Operand::ImmediateAddress(1)),
+        Instruction::Load(Operand::Immediate(4096)),
+        Instruction::Store(Operand::IntermediateAddress(1))
+    ], 512);
+    while !processor.is_halted() {
+        if let Err(message) = processor.execute_instruction() {
+            println!("execution error: {}", message);
+            exit(1);
+        }
+    }
+    processor.dump();
 }
